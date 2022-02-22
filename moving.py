a301c2e8bb47da8c1944206cb93c9d6f77f4583e
Funky Panda <72228432+FunkyPanda3245@users.noreply.github.com>
2022-02-22 13:21:57 +0000

    Update moving.py

--- moving.py
+++ moving.py
@@ -18,7 +18,7 @@ import time
 pushbullet_key = 'o.9r4kfPlL31WmffeqNOmH6wsCLFwLsB08'
 pb = Pushbullet(pushbullet_key)
 api_key = "AIzaSyAzFd8E4KCdYYESa_zgBBGjUE8q7ktQrxk"
-playlist_id = "PLdZNFVCDo_1dPYrlJkQIWlwjRZXQ-r9YN"
+playlist_id = "WHAT EVER PLAYLIST ID"
 
 youtube = build("youtube","v3",developerKey=api_key)
 
@@ -28,6 +28,8 @@ api_version = 'v3'
 request = youtube.playlistItems().list(part=['snippet'], playlistId=playlist_id)
 response = request.execute()
 
+#Gets the latest video Id from playlist
+
 for (k, v) in response.items():
     if(k == 'items'):
         video_ids = [pli['snippet']['resourceId']['videoId'] for pli in v if pli['snippet']['resourceId']['kind']=='youtube#video']
@@ -35,10 +37,13 @@ for (k, v) in response.items():
 youtube_link = "https://www.youtube.com/watch?v="+video_ids[0]
 YDL_OPTIONS={}
 
+
+# This just gets information about the video and you can use that to rename files .etc
+
 with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(youtube_link , download=False)
-
 title = (info["title"])
+
 
 context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
@@ -47,8 +52,6 @@ pushes = pb.get_pushes()
 #pb.push_note("DOWNLOADED:", "IT IS SAFE TO REMOVE NOW")
 
 import os.path
-
-file_exists = os.path.exists('/media/pi/MOVIES/'+(title)+".mp4")
 
 #print(file_exists)
 print ("Insert USB:")
@@ -60,20 +63,12 @@ for device in iter(monitor.poll, None):
         time.sleep(10)
         #subprocess.run(["mount","/dev/sda1","/media/usb/"])
         #time.sleep(2)
-        subprocess.run(["mv","*.mp4","/media/pi/MOVIES"])
+        subprocess.run(["mv","*.mp4","/path/to/USB"])
         #subprocess.run(["umount","/media/usb/"])
         #subprocess.run(["eject","sda1"])        
         pb.push_note("BUG REPORT","The file has Successfully transferd to usb")
         print(file_exists)
         #subprocess.run(["python3", "test.py"])
-
-
-
-
-      		
-        
-
         #pb.push_note("DOWNLOADED:", "IT IS SAFE TO REMOVE NOW")
         #print(title)
         
-#subprocess.run(["mv",title+".mp4","/media/pi/2275-474D/Muma"])
